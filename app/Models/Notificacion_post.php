2d4198ed22ef1a5357649f533fabfde299a9c089
Emmanuel Marcos Gabriel Villalba <evillalba@frba.utn.edu.ar>
2022-11-08 20:07:39 -0300

    agregados los models y los migrations.

--- app/Models/Notificacion_post.php
+++ app/Models/Notificacion_post.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Usuario extends Model
+class Notificacion_post extends Model
 {
     use HasFactory;
 }
--- app/Models/Valoracion_post.php
+++ app/Models/Valoracion_post.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Usuario extends Model
+class Valoracion_post extends Model
 {
     use HasFactory;
 }
