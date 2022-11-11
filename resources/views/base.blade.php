7185974c29753f39cd017cd655b17f9045b37f9b
Emmanuel Marcos Gabriel Villalba <evillalba@frba.utn.edu.ar>
2022-11-11 11:14:51 -0300

    muy buen trabajo.

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,14 +7,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/navbar.css')}}" type="text/css">
-{{--    <link rel="stylesheet" href="{{asset('css/estilos.css')}}" type="text/css">--}}
     <link rel="shortcut icon" href="{{asset('images/favicon.png')}}" type="image/x-icon">
     @stack('styles')
     @stack('scripts')
     @yield('head')
     <title>@yield('titulo')</title>
 </head>
-<body @yield('body_atrrib') class="body">
+<body @yield('body_atrrib')>
 <x-navbar></x-navbar>
 @yield('contenido')
 @stack('scripts')
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,7 @@
 <nav class="navbar static-top navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -14,38 +15,32 @@
                             <a class="nav-item nav-link" href="{{route('usuarios.usuarios')}}">Usuarios</a>
                         </div>
                     </div>
-
                     <div class="container">
                         <div class="">
                             <div class="navbar-nav justify-content-center">
                                 <form class="search-bar" role="search">
-                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                    <input class="form-control me-2" type="search" placeholder="Search"
+                                           aria-label="Search">
                                 </form>
                             </div>
                         </div>
                     </div>
-
                     <div class="container pe-0">
                         <div class="navbar-nav justify-content-end">
-
                             @if(\Illuminate\Support\Facades\Auth::check())
                                 <form class="logout" method="post" action="{{route('login.destroy')}}">
                                     @csrf
                                     <button type="submit" class="nav-item btn btn-primary">Cerrar Sesión</button>
                                 </form>
-
                             @else
-                                <a class="nav-item btn btn-primary me-3" href="{{route('login.create')}}">Iniciar Sesión</a>
+                                <a class="nav-item btn btn-primary me-3" href="{{route('login.create')}}">Iniciar
+                                    Sesión</a>
                                 <a class="nav-item btn btn-outline-primary" href="{{route('usuarios.create')}}">Registrarse</a>
                             @endif
                         </div>
                     </div>
-
-
                 </div>
             </div>
-
         </div>
-
     </div>
 </nav>
